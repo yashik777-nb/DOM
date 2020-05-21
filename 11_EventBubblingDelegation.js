c5af63f8f111a88580e58887d452bc1d304eb024
Yashwanth Iddibani <yaswanth.iddibani@gmail.com>
2020-05-21 23:34:47 +0100

    Event Bubbling and Delegation

---
+++ 11_EventBubblingDelegation.js
@@ -0,0 +1,33 @@
+// Event Bubbling
+// document.querySelector(".card-title").addEventListener("click", function (e) {
+//   console.log(`Event Type: ${e.type} on Card Title`);
+// });
+
+// document.querySelector(".card-content").addEventListener("click", function (e) {
+//   console.log(`Event Type: ${e.type} on Card Content`);
+// });
+
+// document.querySelector(".card").addEventListener("click", function (e) {
+//   console.log(`Event Type: ${e.type} on Card `);
+// });
+
+// document.querySelector(".col").addEventListener("click", function (e) {
+//   console.log(`Event Type: ${e.type} on Col `);
+// });
+
+// Event Delegation
+const deleteItem = document.querySelector(".delete-item");
+deleteItem.addEventListener("click", delItem);
+
+document.body.addEventListener("click", delItem);
+
+function delItem(e) {
+  //   console.log(e.target);
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    console.log("Delete Item");
+    e.target.parentElement.parentElement.remove();
+  }
+  //   if (e.target.parentElement.className == "delete-item secondary-content") {
+  //     console.log("Delete Item");
+  //   }
+}
